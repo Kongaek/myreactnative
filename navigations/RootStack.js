d87e05d6c8d5640284f10d549cf3940a417fc42e
Kongaek <kongaek.seangpo@gmail.com>
2022-05-22 12:57:34 +0700

    WK9

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabs from "./BottomTabs";
 import PokemonTab from "./PokemonTab";
+import Book from "../screens/week9/Book";
+import BookDetail from "../screens/week9/BookDetail";
+import BookForm from "../screens/week9/BookForm";
 
 const Stack = createStackNavigator();
 
@@ -9,7 +12,10 @@ export default function RootStack() {
   return (
     <Stack.Navigator initialRouteName="BottomTabs">
       <Stack.Screen name="BottomTab" component={BottomTabs} options={{ headerShown: false }} />
-      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title: "Pokemon World" }} />
+      <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+      <Stack.Screen name="BookForm" component={BookForm} />
     </Stack.Navigator>
   );
 }
