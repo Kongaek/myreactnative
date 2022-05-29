4cf213c53e5b61953f8f88aa7a5e1cee26eb39df
Kongaek <kongaek.seangpo@gmail.com>
2022-05-29 16:53:49 +0700

    wk12

--- screens/week9/BookDetail.js
+++ screens/week9/BookDetail.js
@@ -1,18 +1,22 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import React, { useEffect, useLayoutEffect } from "react";
-import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { Alert, Image, Text, TouchableOpacity, View,Modal } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import File from "../../helpers/File";
+import ImageViewer from 'react-native-image-zoom-viewer';
 import BookLaravel from "../../services/BookLaravel";
 export default function BookDetail() {
     const route = useRoute();
     const { item } = route.params;
+    const [book, setBook] = useState(item);
+    const [modalVisible, setModalVisible] = useState(false);
     const confirmDelete = () => {
         return Alert.alert(
             "ยืนยันการลบ?",
             "คุณแน่ใจหรือไม่ว่าจะลบรายการนี้?",
             [
                 { text: "ยกเลิก", },
-                { text: "ยืนยัน", onPress: () => { deleteBook();}, },
+                { text: "ยืนยัน", onPress: () => { deleteBook(); }, },
             ]
         );
     };
@@ -20,26 +24,26 @@ export default function BookDetail() {
         //REMOVE BOOK
         // await BookStorage.removeItem(item);
         await BookLaravel.destroyItem(item);
-        
+
         //REDIRECT TO
         navigation.navigate("Book");
-      };
-      useEffect(async()=>{
+    };
+    useEffect(async () => {
         // let b = await BookStorage.readItemDetail(item);
         let b = await BookLaravel.getItemDetail(item);
         setBook(b);
-      },[]);
-    
+    }, []);
+
 
     const navigation = useNavigation();
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
                 <View style={{ flexDirection: "row", width: 100, justifyContent: "space-around" }}>
-                    <TouchableOpacity onPress={() => {navigation.navigate("BookForm", { "item" : item }); }}>
+                    <TouchableOpacity onPress={() => { navigation.navigate("BookForm", { "item": item }); }}>
                         <FontAwesome name="edit" size={30} />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {  confirmDelete();}}>
+                    <TouchableOpacity onPress={() => { confirmDelete(); }}>
                         <FontAwesome name="trash" size={30} />
                     </TouchableOpacity>
                 </View>
@@ -48,14 +52,23 @@ export default function BookDetail() {
     }, [navigation]);
     return (
         <View style={{ backgroundColor: "white", padding: 20, flex: 1 }} >
-            <View style={{ flexDirection: "row" }}>
-                <Image style={{ flex: 1, resizeMode: "contain", aspectRatio: 1 / 1 }} source={{ uri: item.image }} />
-            </View>
+            <TouchableOpacity onPress={() => { setModalVisible(true); }} >
+                <View style={{ flexDirection: "row" }}>
+                    <Image style={{ flex: 1, resizeMode: "contain", aspectRatio: 1 / 1 }} source={{ uri: item.image }} />
+                </View>
+            </TouchableOpacity>
             <Text style={{ fontSize: 20, height: 70, marginVertical: 10 }}> {item.name} </Text>
             <View style={{ flexDirection: "row" }}>
                 <Text style={{ color: "green", fontSize: 20 }}>{item.price}</Text>
                 <Text style={{ paddingTop: 6 }}> บาท</Text>
             </View>
+            <Modal visible={modalVisible} transparent={true} onRequestClose={() => { setModalVisible(false); }} >
+                <ImageViewer imageUrls={[{ url: book.image, props: { } }]}
+                    enableSwipeDown={true}
+                    onCancel={()=>{ console.log("SwipeDown"); setModalVisible(false); }}
+                    onSave={(uri)=>{ File.download(uri);alert("Save File Already!!!!"); }}
+                    />
+            </Modal>
         </View>
     );
 }
